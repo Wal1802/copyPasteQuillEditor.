1b491fc096fbbc59221e9b76edd175b894f790f5
walddry Enrique <walddry@gmail.com>
2019-02-13 15:00:53 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <quill-editor v-model="texto" :options="editorOption"></quill-editor>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      texto: "hola",
+      editorOption: {
+        placeholder: "Inserte el texto aquí",
+        modules: {
+          //imageDrop: true,
+          imagePaste: true,
+          toolbar: [
+            ["bold", "italic", "underline", "strike"],
+            ["blockquote", "code-block"],
+            [{ header: 1 }, { header: 2 }],
+            [{ list: "ordered" }, { list: "bullet" }],
+            [{ script: "sub" }, { script: "super" }],
+            [{ indent: "-1" }, { indent: "+1" }],
+            [{ direction: "rtl" }],
+            [{ size: ["small", false, "large", "huge"] }],
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            [{ font: [] }],
+            [{ color: [] }, { background: [] }],
+            [{ align: [] }],
+            ["clean"],
+            ["link", "image", "video"],
+            ["template-partediario"]
+          ]
+        }
+      }
+    };
+  }
+};
+</script>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import App from "./App";
+
+Vue.config.productionTip = false;
+
+import VueQuillEditor from "vue-quill-editor";
+import Quill from "quill";
+
+import { ImageDrop } from "quill-image-drop-module";
+
+import { ImagePaste } from "./imagePasteClass";
+
+Quill.register("modules/imageDrop", ImageDrop);
+Quill.register("modules/imagePaste", ImagePaste);
+
+Vue.use(VueQuillEditor);
+Vue.component("quill-editor", VueQuillEditor.quillEditor);
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  components: { App },
+  template: "<App/>"
+});
